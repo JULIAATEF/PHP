455c08bf2892a31caf2d4648f8fdd8fc92e26f2a
JULIAATEF <116893041+JULIAATEF@users.noreply.github.com>
2023-12-12 15:58:33 +0200

    Add files via upload

---
+++ data.php
@@ -0,0 +1,12 @@
+<?php
+echo $_POST["fname"]."<br>";
+echo $_POST["lname"]."<br>";
+echo $_POST["Address"]."<br>";
+echo $_POST["country"]."<br>";
+echo $_POST["GENDER"]."<br>";
+echo $_POST["skill"]."<br>";
+
+
+
+
+?>
